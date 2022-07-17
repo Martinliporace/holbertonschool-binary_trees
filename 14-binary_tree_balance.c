5e8735adeb1789dcb14a7d01ce352a09e33e46b3
root <martin.liporace@gmail.com>
2022-07-17 10:27:47 -0700

    height function fixed

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -28,14 +28,21 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int height_l = binary_tree_height(tree->left);
-	int height_r = binary_tree_height(tree->right);
+	size_t height_l = 0;
+	size_t height_r = 0;
 
 	if (tree == NULL)
 		return (0);
 
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
+	height_l = binary_tree_height(tree->left) + 1;
+	height_r = binary_tree_height(tree->right) + 1;
+
 	if (height_l >= height_r)
-		return (height_l + 1);
+		return (height_l);
+
 	else
-		return (height_r + 1);
+		return (height_r);
 }
