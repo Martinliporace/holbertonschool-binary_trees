ebbdd4f6db4f81efac561def0da3e65033b348bf
root <mcarrascopiaggio@gmail.com>
2022-07-17 08:19:50 -0700

    change order of NULL cases

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,12 +11,13 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode;
 
+	if (parent == NULL)
+		return (NULL);
+
 	newNode = malloc(sizeof(binary_tree_t));
 	if (newNode == NULL)
 		return (NULL);
 
-	if (parent == NULL)
-		return (NULL);
 	newNode->parent = parent;
 	newNode->n = value;
 	newNode->left = NULL;
