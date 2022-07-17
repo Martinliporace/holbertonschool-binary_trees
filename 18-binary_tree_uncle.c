4cc4594bd408f8ec417df8e17ae6fb5e35d235d1
root <martin.liporace@gmail.com>
2022-07-17 09:15:45 -0700

    18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,18 +9,18 @@
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 
 {
+	binary_tree_t *grandparent = node->parent->parent;
+
 	if (!node || node->parent->parent == NULL)
 		return (NULL);
 
-	binary_tree_t *grandparent = node->parent->parent;
+	if (grandparent->left != node->parent)
+		return (grandparent->left);
 
-		if (grandparent->left != node->parent)
-			return (grandparent->left);
+	if (grandparent->right != node->parent)
+		return (grandparent->right);
 
-		if (grandparent->right != node->parent)
-			return (grandparent->right);
-
-		else
-			return (NULL);
+	else
+		return (NULL);
 
 }
