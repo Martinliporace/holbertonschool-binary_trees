2e7a73d7797a4e417b1463b087fe88d12f299070
root <martin.liporace@gmail.com>
2022-07-17 09:35:20 -0700

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,11 +11,27 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	int height_l = binary_tree_height(tree->left);
 	int height_r = binary_tree_height(tree->right);
 
-	if (tree == NULL)
+	if (tree == NULL || binary_tree_is_leaf(tree))
 		return (0);
 
 	if (height_r >= height_l)
 		return (height_r + 1);
 
 	return (height_r + 1);
+}
+
+/**
+* binary_tree_is_leaf - checks if a node is a leaf
+* @node: Integer stored in the node
+* Return: 1 if node is a leaf, otherwise 0
+*/
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	else
+		return (0);
 }
