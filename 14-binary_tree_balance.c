0a75f809b6f0a9499f96aca14d5ad577708cd833
root <martin.liporace@gmail.com>
2022-07-17 08:31:29 -0700

    14 betty fxd

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_height - measures the height of a binary tree
+* binary_tree_balance - measures the balance factor of a binary tree
 * @tree: a pointer to the root node of the tree to measure the height.
 * Return: the height of a binary tree or 0 if tree is NULL
 */
