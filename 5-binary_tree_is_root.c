d1190548c08b583790c7a8cdb416eb46d04e6e74
root <mcarrascopiaggio@gmail.com>
2022-07-17 08:40:21 -0700

    change order of var declaration

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
-* int binary_tree_is_root- checks if a node is a leaf
+* binary_tree_is_root- checks if a node is a leaf
 * @node: Integer stored in the node
 * Return: 1 if node is a root, otherwise 0
 */
@@ -11,8 +11,8 @@ int binary_tree_is_root(const binary_tree_t *node)
 	if (node == NULL)
 		return (0);
 
-		if (node->parent == NULL)
-			return (1);
-		else
-			return (0);
+	if (node->parent == NULL)
+		return (1);
+	else
+		return (0);
 }
