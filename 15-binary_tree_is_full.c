9b4c7686aae43ee81e243641fffbb5ccb29727c3
root <mcarrascopiaggio@gmail.com>
2022-07-17 12:40:30 -0700

    add NULL condition

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,7 +9,7 @@
 int binary_tree_is_full(const binary_tree_t *tree)
 
 {
-	if (tree == NULL)
+	if (tree == NULL || tree->parent == NULL)
 		return (1);
 
 	if (tree->left == NULL && tree->right == NULL)
