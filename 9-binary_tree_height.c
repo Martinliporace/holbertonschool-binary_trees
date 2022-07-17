4999f4ab082e360df396ae712187135117839d77
root <martin.liporace@gmail.com>
2022-07-17 10:23:59 -0700

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,31 +8,21 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-
-	return (tree_height_aux(tree) - 1);
-}
-
-/**
-* tree_height_aux - auxiliar function for tree height
-* @tree: a pointer to the root node of the tree to measure the height.
-* Return: the height of a binary tree or 0 if tree is NULL
-*/
-
-size_t tree_height_aux(const binary_tree_t *tree)
-{
 	size_t height_l = 0;
 	size_t height_r = 0;
 
-	height_l = tree_height_aux(tree->left);
-	height_r = tree_height_aux(tree->right);
-
 	if (tree == NULL)
 		return (0);
 
-	if (height_l > height_r)
-		return (height_l + 1);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
 
-	return (height_r + 1);
+	height_l = binary_tree_height(tree->left) + 1;
+	height_r = binary_tree_height(tree->right) + 1;
+
+	if (height_l >= height_r)
+		return (height_l);
+
+	else
+		return (height_r);
 }
