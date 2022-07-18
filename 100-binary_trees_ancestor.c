ae55c347c1f0ec63e6a1f16362d8390be69aebcd
root <martin.liporace@gmail.com>
2022-07-18 07:00:46 -0700

    100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -57,7 +57,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
-		return (1);
+		return (0);
 
 	height_l = binary_tree_height(tree->left) + 1;
 	height_r = binary_tree_height(tree->right) + 1;
